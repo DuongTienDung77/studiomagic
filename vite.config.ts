821d682b799ef1b30e649de76ddf49e3b0c20167
DuongTienDung77 <59427327+DuongTienDung77@users.noreply.github.com>
2025-09-06 21:28:42 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.AIzaSyCnIlyGlLsQuiQH_rq3MvtmrN2xhZDfM8U),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.AIzaSyCnIlyGlLsQuiQH_rq3MvtmrN2xhZDfM8U)
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    define: {
+      "import.meta.env.VITE_GEMINI_API_KEY": JSON.stringify(env.AIzaSyCnIlyGlLsQuiQH_rq3MvtmrN2xhZDfM8U),
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "."),
       },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+    },
+  };
 });
