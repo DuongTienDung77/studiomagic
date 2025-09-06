271c0e9746770793198717df527f6b90a556b9de
DuongTienDung77 <59427327+DuongTienDung77@users.noreply.github.com>
2025-09-06 21:30:36 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import path from "path";
 import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, process.cwd(), ""); // lấy .env.* + Netlify env
 
   return {
     define: {
-      "import.meta.env.VITE_GEMINI_API_KEY": JSON.stringify(env.AIzaSyCnIlyGlLsQuiQH_rq3MvtmrN2xhZDfM8U),
+      // expose biến cho client
+      "import.meta.env.VITE_GEMINI_API_KEY": JSON.stringify(env.VITE_GEMINI_API_KEY),
     },
     resolve: {
       alias: {
